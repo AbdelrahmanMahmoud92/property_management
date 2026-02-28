1411058fe5f57c4ec44c1c0fc2478058a995e8da
AbdelrahmanMahmoud92 <azarena818@gmail.com>
2026-02-28 14:53:41 +0200

    Customize doctypes (Subscription plan&Item)

---
+++ property_management/property_management/doctype/unit_type/unit_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Abdelrahman Mahmoud and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Unit Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
